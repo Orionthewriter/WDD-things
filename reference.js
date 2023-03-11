0611b94f89cf1fd259dd94683423fdf2b4f95a64
Sam Running <famousodin12345@gmail.com>
2023-03-11 16:00:50 -0700

    1yrs

---
+++ reference.js
@@ -0,0 +1,36 @@
+function mysearchFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+function fillTable() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = document.getElementById("userID").value;
+    cell2.innerHTML = document.getElementById("firstname").value;
+    cell3.innerHTML = document.getElementById("lastname").value;
+    cell4.innerHTML = document.getElementById("LOGO").value;
+    return false;
+}
+function myDeleteFunction() {
+    document.getElementById("myTable").deleteRow(0);}
